9831d9575e816110e8a947ccded28bcb5d58d3f8
omiplekevin <omiple.kevin@gmail.com>
2014-02-09 01:50:11 +0800

    change in directory

--- herbal/fuel/app/logs/2014/02/06.php
+++
@@ -1,3 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2014-02-06 11:29:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
