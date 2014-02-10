a8c96547a7d27b670d18c437d69ba43468952965
omiplekevin <omiple.kevin@gmail.com>
2014-02-10 09:33:04 +0800

    mod

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,9 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=herbal',
-			'username'   => 'root',
-			'password'   => '',
+			'dsn'        => 'mysql:host=tunnel.pagodabox.com:3306;dbname=herbal',
+			'username'   => 'isabell',
+			'password'   => 'k0CVc2la',
 		),
 	),
 );
